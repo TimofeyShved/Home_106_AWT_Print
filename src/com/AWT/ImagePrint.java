38ed1bae116964b18c26fd4d9584d57cd165b4e1
timofeya123 <timofeya123@inbox.ru>
2021-05-28 15:39:22 +0600

    Print_Image_Comment

--- src/com/AWT/ImagePrint.java
+++ src/com/AWT/ImagePrint.java
@@ -9,13 +9,19 @@ import java.io.InputStream;
 public class ImagePrint {
 
     public static void main(String[] args) throws PrinterException, FileNotFoundException, PrintException {
-        DocFlavor flavor = DocFlavor.INPUT_STREAM.JPEG;
+        DocFlavor flavor = DocFlavor.INPUT_STREAM.JPEG; //формат входящего потока
+
+        // Сервисы для печяти, в каком формате и атрибуты(кол-во копий, настройки печати, качество)
         PrintService[] services = PrintServiceLookup.lookupPrintServices(flavor, null);
+
+        // файл, который мы будем печятать
         InputStream inputStream = new FileInputStream("1.jpg");
+
+        //настройки, (файл, формат, атрибуты)
         Doc doc = new SimpleDoc(inputStream, flavor, null);
         if (services.length>0){
-            DocPrintJob job = services[0].createPrintJob();
-            job.print(doc, null);
+            DocPrintJob job = services[0].createPrintJob(); // добавляем работу печяти
+            job.print(doc, null); // распечятка документа
         }
     }
 }
