4a7b3971fead7eb490c737ed47ea6a58d9fe89e7
timofeya123 <timofeya123@inbox.ru>
2021-05-29 21:26:52 +0600

    PostScript_comment

--- src/com/AWT/PostScript.java
+++ src/com/AWT/PostScript.java
@@ -6,15 +6,17 @@ import java.io.*;
 
 public class PostScript {
     public static void main(String[] args) throws FileNotFoundException, PrintException {
-        DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PRINTABLE;
-        String mimeType = "application/postscript";
+        DocFlavor flavor = DocFlavor.SERVICE_FORMATTED.PRINTABLE; //формат входящего потока
+        String mimeType = "application/postscript"; // тип жокумента
+        // переработка потока в нужный тип
         StreamPrintServiceFactory[] factories = StreamPrintServiceFactory.lookupStreamPrintServiceFactories(flavor, mimeType);
-        OutputStream outputStream = new FileOutputStream("PostService.odd");
-        StreamPrintService streamPrintService = factories[0].getPrintService(outputStream);
+        OutputStream outputStream = new FileOutputStream("PostService.odd"); // выходной поток
+        StreamPrintService streamPrintService = factories[0].getPrintService(outputStream);  // переводим поток для вывода на печять
 
+        // входящий поток картинки
         InputStream inputStream = new FileInputStream("1.jpg");
-        DocPrintJob job = streamPrintService.createPrintJob();
-        Doc doc = new SimpleDoc(inputStream, DocFlavor.INPUT_STREAM.JPEG, null);
-        job.print(doc, null);
+        DocPrintJob job = streamPrintService.createPrintJob();  // работа печяти
+        Doc doc = new SimpleDoc(inputStream, DocFlavor.INPUT_STREAM.JPEG, null);//настройки, (файл, формат, атрибуты)
+        job.print(doc, null); // печять
     }
 }
